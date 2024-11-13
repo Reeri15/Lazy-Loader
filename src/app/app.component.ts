1219e4ef3f02df2a0aa204ef79551cba5242dc69
reeri15 <reshay.makunyane@gmail.com>
2024-11-13 09:05:36 +0200

    Lazy-load methodd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { NavComponent } from "./pages/sharedUI/nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app';
+
+  isLoad = false
+  onShow(){
+  this.isLoad = !this.isLoad
+
+  }
 }
